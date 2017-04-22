ca8104ba3e7b7b1f4ddc4ba0465d215935156633
ddmilo <ddmilo1029@gmail.com>
2017-04-22 18:18:02 -0400

    added flex to about section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ window.onload = function(){
 $('.collapse').collapse({
 	toggle: false
 });
+
+$().ready(function(){
+		 $('[rel="tooltip"]').tooltip();
+
+ });
+
+ function rotateCard(btn){
+		 var $card = $(btn).closest('.card-container');
+		 console.log($card);
+		 if($card.hasClass('hover')){
+				 $card.removeClass('hover');
+		 } else {
+				 $card.addClass('hover');
+		 }
+ }
